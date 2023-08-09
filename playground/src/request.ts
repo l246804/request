101b0561089686894b65943ba121d2a64cfcd2bb
Ray Hao <2468048176@qq.com>
2023-08-09 11:34:12 +0800

    chore: 功能测试

--- playground/src/request.ts
+++ playground/src/request.ts
@@ -1,8 +1,8 @@
-import { createRequest } from '@rhao/request/index'
-import { createMiddleware } from '@rhao/request-basic-middleware/index'
-import { RequestVue } from '@rhao/request-middleware-vue/index'
+import { createRequest } from '@rhao/request'
+import { RequestSWR } from '@rhao/request-basic-middleware/swr'
+import { RequestVue } from '@rhao/request-middleware-vue'
 
 export const useRequest = createRequest({
   manual: true,
-  middleware: [RequestVue(), ...createMiddleware({ refresh: { errorRetryCount: 3 } })],
+  middleware: [RequestVue(), RequestSWR()],
 })
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,19 +1,16 @@
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ['@rhao/request', '@rhao/request-basic-middleware', '@rhao/request-middleware-vue'],
+  },
   build: {
     minify: false,
     sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@rhao/request': resolve(__dirname, '../packages/request/src'),
-      '@rhao/request-utils': resolve(__dirname, '../packages/utils/src'),
-      '@rhao/request-basic-middleware': resolve(__dirname, '../packages/basic-middleware/src'),
-      '@rhao/request-middleware-vue': resolve(__dirname, '../packages/middleware-vue/src'),
+    commonjsOptions: {
+      include: [/@rhao\/.+/, /node_modules/],
     },
   },
   plugins: [vue()],
