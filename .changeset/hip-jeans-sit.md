c4c69be7552432c87e3edcb4604ef52a5e67ce0a
Ray Hao <2468048176@qq.com>
2023-10-10 13:44:01 +0800

    docs(changeset): feat: request 组合函数支持单次触发

---
+++ .changeset/hip-jeans-sit.md
@@ -0,0 +1,5 @@
+---
+'@rhao/request': minor
+---
+
+feat: request 组合函数支持单次触发
--- packages/request/src/builtin-middleware/composables.ts
+++ packages/request/src/builtin-middleware/composables.ts
@@ -20,7 +20,19 @@ export function RequestComposables() {
         'dispose',
       ]
       const composables = composableConfigHooks.reduce((obj, hookKey) => {
-        obj[camelCase(`on-${hookKey}`)] = (cb) => hooks.hook(hookKey as any, cb)
+        obj[camelCase(`on-${hookKey}`)] = (cb, options?: ComposableFnOptions) => {
+          const { once = false } = options || {}
+          if (once) {
+            const _cb = cb
+            cb = function (...args) {
+              // eslint-disable-next-line @typescript-eslint/no-use-before-define
+              fn()
+              return _cb(...args)
+            }
+          }
+          const fn = hooks.hook(hookKey as any, cb)
+          return fn
+        }
         return obj
       }, {} as Recordable<AnyFn>)
 
@@ -33,45 +45,79 @@ export function RequestComposables() {
 }
 
 // 类型别名，防止导入类型被忽视
-type ConfigHooks<TData, TParams extends unknown[]= unknown[]> = RequestConfigHooks<TData, TParams>
+type ConfigHooks<TData, TParams extends unknown[] = unknown[]> = RequestConfigHooks<TData, TParams>
+
+interface ComposableFnOptions {
+  /**
+   * 是否仅单次触发
+   */
+  once?: boolean
+}
 
 declare module '@rhao/request' {
   export interface RequestResult<TData, TParams extends unknown[] = unknown[]> {
     /**
      * `hook:preface`
      */
-    onPreface: Fn<[callback: ConfigHooks<TData, TParams>['preface']], NoopFn>
+    onPreface: Fn<
+      [callback: ConfigHooks<TData, TParams>['preface'], options?: ComposableFnOptions],
+      NoopFn
+    >
     /**
      * `hook:before`
      */
-    onBefore: Fn<[callback: ConfigHooks<TData, TParams>['before']], NoopFn>
+    onBefore: Fn<
+      [callback: ConfigHooks<TData, TParams>['before'], options?: ComposableFnOptions],
+      NoopFn
+    >
     /**
      * `hook:success`
      */
-    onSuccess: Fn<[callback: ConfigHooks<TData, TParams>['success']], NoopFn>
+    onSuccess: Fn<
+      [callback: ConfigHooks<TData, TParams>['success'], options?: ComposableFnOptions],
+      NoopFn
+    >
     /**
      * `hook:error`
      */
-    onError: Fn<[callback: ConfigHooks<TData, TParams>['error']], NoopFn>
+    onError: Fn<
+      [callback: ConfigHooks<TData, TParams>['error'], options?: ComposableFnOptions],
+      NoopFn
+    >
     /**
      * `hook:after`
      */
-    onAfter: Fn<[callback: ConfigHooks<TData, TParams>['after']], NoopFn>
+    onAfter: Fn<
+      [callback: ConfigHooks<TData, TParams>['after'], options?: ComposableFnOptions],
+      NoopFn
+    >
     /**
      * `hook:finally`
      */
-    onFinally: Fn<[callback: ConfigHooks<TData, TParams>['finally']], NoopFn>
+    onFinally: Fn<
+      [callback: ConfigHooks<TData, TParams>['finally'], options?: ComposableFnOptions],
+      NoopFn
+    >
     /**
      * `hook:cancel`
      */
-    onCancel: Fn<[callback: ConfigHooks<TData, TParams>['cancel']], NoopFn>
+    onCancel: Fn<
+      [callback: ConfigHooks<TData, TParams>['cancel'], options?: ComposableFnOptions],
+      NoopFn
+    >
     /**
      * `hook:discarded`
      */
-    onDiscarded: Fn<[callback: ConfigHooks<TData, TParams>['discarded']], NoopFn>
+    onDiscarded: Fn<
+      [callback: ConfigHooks<TData, TParams>['discarded'], options?: ComposableFnOptions],
+      NoopFn
+    >
     /**
      * `hook:dispose`
      */
-    onDispose: Fn<[callback: ConfigHooks<TData, TParams>['dispose']], NoopFn>
+    onDispose: Fn<
+      [callback: ConfigHooks<TData, TParams>['dispose'], options?: ComposableFnOptions],
+      NoopFn
+    >
   }
 }
